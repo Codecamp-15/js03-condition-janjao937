c9b5db1605adc4d3b71645d56cfd4e4e0988e8c5
janjao937 <janjao937@gmail.com>
2023-08-08 20:36:54 +0700

    fiexd loginFlow

--- 01-If-Else/07-loginFlow/main.js
+++ 01-If-Else/07-loginFlow/main.js
@@ -4,16 +4,50 @@
 - หาก username = “admin” และ password = “1234” หรือ username = “john” และ password = “qwerty” ให้โชว์ข้อความ “Hello” ตามด้วยชื่อ username
 - หาก username และ password ไม่ตรงตามเงื่อนไขที่กล่าวมาให้ โชว์ข้อความ “invalid username or password”
 */
+let userName;
+let password;
 
-let userName = prompt("User Name");
-let password = prompt("Password");
-let notiText = "username is required";
-
-if((userName || password) === null)
+userName = prompt("User Name");
+if(userName===null||userName ==="")
 {
-    userName === null?alert("username is required"):alert("password is required");
-} 
-else if((userName === "admin" || userName === "joe")&& ( password === "1234" || password === "qwerty"))
+    alert("username is required");
+}
+password = prompt("Enter Password");
+if(password===null||password ==="")
+{
+    alert("password is required");
+}
+
+if((userName ==="admin"&&password==="1234")||(userName==="john" && password==="qwerty"))
+{
+    alert(`Hello ${userName}`);
+}
+else
+{
+    alert("Invalid username or password");
+}
+
+/*
+let userName = prompt("User Name");
+let password;
+
+let notiText = "username is required";
+if(userName === null|| " ")
+{
+    alert("username is required");
+}
+else
+{
+    let password = prompt("Password");
+}
+
+
+if(userName === null|| " ")
+{
+    alert("password is required");
+}
+
+if((userName === "admin" || userName === "joe")&& ( password === "1234" || password === "qwerty"))
 {
     alert("Hello "+userName);
 }
@@ -21,3 +55,7 @@ else
 {
     alert("invalid username or password");
 }
+*/
+
+////// อย่าลืมำไปแก้จ้าาาา ///////
+
